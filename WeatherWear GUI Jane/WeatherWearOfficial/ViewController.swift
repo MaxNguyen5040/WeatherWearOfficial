56b3421c3ecc9059cf274ea7f407c7fa1dddf4cb
jebuic <87820855+jebuic@users.noreply.github.com>
2021-07-27 10:55:32 -0500

    Add files via upload
    Added Forecast tab

---
+++ WeatherWear GUI Jane/WeatherWearOfficial/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  WeatherWearOfficial
+//
+//  Created by Max Nguyen on 7/22/21.
+//  41.799688 -88.111088
+
+import UIKit
+
+class ViewController: UIViewController {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
